1355f47f794d26089a2302a09752db656cff5f55
unknown <sergeypyatin27@gmail.com>
2026-04-23 21:21:51 +0300

    admin regions

---
+++ src/main/java/com/example/backend/RegionController.java
@@ -0,0 +1,33 @@
+package com.example.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/regions")
+public class RegionController {
+
+    @Autowired
+    private RegionRepository regionRepository;
+
+    @GetMapping
+    public List<Region> getAllRegions() {
+        return regionRepository.findAllOrdered();
+    }
+
+    @PostMapping
+    public Region createRegion(@RequestBody Region region) {
+        return regionRepository.save(region);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Region> updateRegion(@PathVariable Integer id, @RequestBody Region regionDetails) {
+        return regionRepository.findById(id).map(region -> {
+            region.setName(regionDetails.getName());
+            return ResponseEntity.ok(regionRepository.save(region));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+}
